52d4fb62bc6f38f926b84f9d2f87fd88838d7a61
Sagnik Chakraborty <sagnikchakss@gmail.com>
2022-01-26 19:51:44 +0530

    stage 02 train and test split

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -9,10 +9,14 @@ def read_yaml(path_to_yaml:str)->dict:
         content=yaml.safe_load(yaml_file)
     return content    
     
- 
 def create_directory(dirs:list):
     #we pass list of directory which we want to create
     for directory_path in dirs:
         os.makedirs(directory_path, exist_ok=True)
         print(f"directory is saved at {directory_path}")    
-  
+
+
+def save_local_df(data,data_path,index_status=False):
+    #to save test train file
+    data.to_csv(data_path,index=index_status)
+    print(f"data is saved at {data_path}")  
